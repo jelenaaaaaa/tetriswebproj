23339be4f27b9060a1c9ecfc8e47bea914c8816d
yel00 <51717671+jelenaaaaaa@users.noreply.github.com>
2019-07-02 10:25:50 +0200

    level comments included

--- ajax.js
+++ ajax.js
@@ -1,6 +1,14 @@
 document.getElementById("showbutton").addEventListener('click',function(){loadData(1,'showScores')});
 document.getElementById("showbuttonlast").addEventListener('click',function(){loadData(2,'showScoresLast')});
 
+/*
+When the level is included in the database
+
+ document.getElementById("showbutton").addEventListener('click',function(){loadData(1,'showScores','showLevels')});
+ document.getElementById("showbuttonlast").addEventListener('click',function(){loadData(2,'showScoresLast','showLevelsLast')});
+
+*/
+
 function insertData() // this is used for inserting data
 {
     var xmlhttp;
@@ -18,15 +26,19 @@ function insertData() // this is used for inserting data
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
             document.getElementById("showAllScores").innerHTML=xmlhttp.responseText;
+            // document.getElementById("showAllLevels").innerHTML=xmlhttp.responseText;
         }
     };
 
     var uname=name;
     var currentscore=document.cookie;
+    // var currentlevel=document.cookie1;
 
     xmlhttp.open("POST","insert_scores.php",true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("uname="+uname+"&currentscore="+currentscore);
+
+    // xmlhttp.send("uname="+uname+"&currentscore="+currentscore+"&currentlevel"+currentlevel);
 }
 
 function loadData(sql,div) //function for displaying data
@@ -58,9 +70,11 @@ function loadData(sql,div) //function for displaying data
 function clearData1() //functions for clearing out the divs
 {
     document.getElementById('showScores').innerHTML = "";
+    //document.getElementById('showLevels').innerHTML = "";
 }
 
 function clearData2()
 {
     document.getElementById('showScoresLast').innerHTML = "";
+    //document.getElementById('showLevelsLast').innerHTML = "";
 }
