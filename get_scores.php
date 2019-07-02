293cd1452aa44c61d7b8f25a233f62017ef15e9a
yel00 <51717671+jelenaaaaaa@users.noreply.github.com>
2019-07-02 10:27:02 +0200

    level comments included

--- get_scores.php
+++ get_scores.php
@@ -3,7 +3,13 @@ include("db_config.php");
 $sql = "";
 
 $sqls[1] = "SELECT * FROM userdata ORDER BY score DESC LIMIT 5";
-$sqls[2] = "SELECT * FROM userdata ORDER BY id DESC LIMIT 10";
+
+/*
+ When level is included in the database:
+$sqls[1] = "SELECT * FROM userdata ORDER BY score,level DESC LIMIT 5";
+ */
+
+$sqls[2] = "SELECT * FROM userdata ORDER BY id DESC LIMIT 5";
 
 
 if(isset($_GET['sql']))
@@ -17,6 +23,13 @@ if (mysqli_num_rows($result)>0)
     while ($record = mysqli_fetch_array($result,MYSQLI_BOTH))
     {
         echo "user: $record[username],<br>score: $record[score]<br><hr style='width: 10%'>";
+
+        /*
+         When level is included in the database:
+
+        echo "user: $record[username],<br>score: $record[score],<br>level: $level[level]<br><hr style='width: 10%'>";
+
+         */
     }
 }
 
