9d9ec209be49b26631f0b67aac2bfe16fe9a5e0d
yel00 <51717671+jelenaaaaaa@users.noreply.github.com>
2019-07-02 10:21:17 +0200

    added level comments

--- insert_scores.php
+++ insert_scores.php
@@ -2,6 +2,7 @@
 include("db_config.php");
 $name = "";
 $score = "";
+//$level = "";
 
 if(isset($_POST['uname']))
     $name=mysqli_real_escape_string($connection,$_POST['uname']);
@@ -9,9 +10,20 @@ if(isset($_POST['uname']))
 if(isset($_POST['currentscore']))
     $score=mysqli_real_escape_string($connection,$_POST['currentscore']);
 
+/*
+if(isset($_POST['currentlevel']))
+    $level=mysqli_real_escape_string($connection,$_POST['currentlevel']);
+*/
+
 if(!empty($name))
 {
     $sql = "INSERT INTO userdata(username,score,datetime) VALUES ('$name','$score',CURRENT_TIME)";
+        /*
+         When level is included in the database:
+
+        $sql = "INSERT INTO userdata(username,score,datetime,level) VALUES ('$name','$score',CURRENT_TIME,'$level')";
+
+         */
     $result= mysqli_query($connection,$sql) or die(mysqli_error($connection));
     echo "<br>Your score has been remembered!";
 }
@@ -19,7 +31,5 @@ if(!empty($name))
 else{
     echo "empty!<br>";
 }
-
-
 
 ?>
